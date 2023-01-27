8f7d2697a1616a1ff2151c0b666d0eeb4219b04f
mxnkarou <mxnkarou@icloud.com>
2023-01-27 04:08:53 +0100

    chore: updated repo setup

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,7 +3,6 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra();
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
+module.exports = withNextra({
+  reactStrictMode: true,
+});
--- apps/web/jest.setup.ts
+++ apps/web/jest.setup.ts
@@ -2,5 +2,3 @@ import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('next/router', () => require('next-router-mock'));
-
-process.env.APP_RELEASE_STAGES = 'test';
